9e06911f04bc57e7863778c61f04cefeca9986ac
huelton <hueltondihon@gmail.com>
2020-01-02 15:58:34 -0300

    implementando teste com UserRepository

---
+++ src/main/java/com/wallet/entity/User.java
@@ -0,0 +1,29 @@
+package com.wallet.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class User implements Serializable {
+    
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(nullable = false)
+	private String password;
+	@Column(nullable = false)
+	private String name;
+	private String email;
+}
